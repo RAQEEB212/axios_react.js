64ed3f3cd5c84a0e2d9dc415a59f4529a4789aa7
RAQEEB212 <saneelmemon45@gmail.com>
2024-08-22 18:36:32 +0500

    practing axios lib

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useEffect,useState} from "react";
+import axios from "axios";
 
 function App() {
+  const [data,setData] = useState([])
+  useEffect(() => {
+    axios.get('https://reqres.in/api/users?page=2')
+        .then(res => setData(res.data.data))
+        .then(err => console.log(err));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> API integration in react by axios lib </h1>
+      {
+        data.map((d,i) =>{
+          return <p key={i} > {d.email}</p>
+        })
+      }
     </div>
   );
 }
